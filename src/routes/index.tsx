822b13133bceef196e5c8e8be9d7528c0cbcd2af
Manassarn Manoonchai <manassarn@gmail.com>
2023-05-31 01:41:40 +0700

    Add meta

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,7 @@
 import { component$ } from "@builder.io/qwik"
 import type { DocumentHead } from "@builder.io/qwik-city"
 
-import Counter from "~/components/starter/counter/counter"
 import Hero from "~/components/starter/hero/hero"
-import Infobox from "~/components/starter/infobox/infobox"
-import Starter from "~/components/starter/next-steps/next-steps"
-import { TailwindExample } from "~/components/starter/tailwind/tailwind-example"
 
 export default component$(() => {
   return (
@@ -16,11 +12,11 @@ export default component$(() => {
 })
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Sunny Pass - แคล้วคลาดพ้นภัย",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Sunny Pass - แคล้วคลาดพ้นภัย",
     },
   ],
 }
